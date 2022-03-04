63dc75d2f2ce349aa89a0756a3ecbc0a464c3119
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2022-03-04 10:49:43 -0300

    Atualiza

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react"
-import { View, StyleSheet } from "react-native"
+import { StyleSheet, SafeAreaView } from "react-native"
 
-import ContadorV2 from './components/contador/ContadorV2'
+import ParImpar from './components/ParImpar'
+//import Diferenciar from './components/Diferenciar'
+//import ContadorV2 from './components/contador/ContadorV2'
 //import Pai from './components/indireta/Pai'
 //import Pai from './components/direta/Pai'
 //import Contador from './components/Contador'
@@ -14,9 +16,11 @@ import ContadorV2 from './components/contador/ContadorV2'
 
 
 export default () => (
-    <View style={style.App}>
-        <ContadorV2/>
+    <SafeAreaView style={style.App}>
+        <ParImpar num={8}/>
         {/*
+        <Diferenciar/>
+        <ContadorV2/>
         <Pai/>
         <Pai/>
         <Contador inicial={100} passo={5}/>
@@ -31,7 +35,7 @@ export default () => (
         <Comp2 />
        <Primerio /> 
        */}
-    </View>)
+    </SafeAreaView>)
 
 
 const style = StyleSheet.create({
